8c7bd4a2286ccc0ae826195a62272960c01cd13c
nehalist <github@nehalist.io>
2018-05-30 12:01:54 +0200

    Added basic plugin structure

--- WirecardShopwareElasticEngine/Components/Payments/PaymentInterface.php
+++ WirecardShopwareElasticEngine/Components/Payments/PaymentInterface.php
@@ -29,4 +29,22 @@
  * Please do not use the plugin if you do not agree to these terms of use!
  */
 
-require_once __DIR__ . '/../vendor/autoload.php';
+namespace WirecardElasticEngine\Components\Payments;
+
+interface PaymentInterface
+{
+    /**
+     * @return string
+     */
+    public function getLabel();
+
+    /**
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * @return array
+     */
+    public function getPaymentOptions();
+}
