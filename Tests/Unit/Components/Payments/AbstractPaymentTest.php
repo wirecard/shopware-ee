7c099ff76b9db23c5bc48907a1a5eb4373e4450c
nehalist <github@nehalist.io>
2018-06-05 14:09:57 +0200

    Added abstract payment test

---
+++ Tests/Unit/Components/Payments/AbstractPaymentTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Shop System Plugins - Terms of Use
+ *
+ * The plugins offered are provided free of charge by Wirecard AG and are explicitly not part
+ * of the Wirecard AG range of products and services.
+ *
+ * They have been tested and approved for full functionality in the standard configuration
+ * (status on delivery) of the corresponding shop system. They are under General Public
+ * License version 3 (GPLv3) and can be used, developed and passed on to third parties under
+ * the same terms.
+ *
+ * However, Wirecard AG does not provide any guarantee or accept any liability for any errors
+ * occurring when used in an enhanced, customized shop system configuration.
+ *
+ * Operation in an enhanced, customized configuration is at your own risk and requires a
+ * comprehensive test phase by the user of the plugin.
+ *
+ * Customers use the plugins at their own risk. Wirecard AG does not guarantee their full
+ * functionality neither does Wirecard AG assume liability for any disadvantages related to
+ * the use of the plugins. Additionally, Wirecard AG does not guarantee the full functionality
+ * for customized shop systems or installed plugins of other vendors of plugins within the same
+ * shop system.
+ *
+ * Customers are responsible for testing the plugin's functionality before starting productive
+ * operation.
+ *
+ * By installing the plugin into the shop system the customer agrees to these terms of use.
+ * Please do not use the plugin if you do not agree to these terms of use!
+ */
+
+namespace WirecardShopwareElasticEngine\Tests\Functional\Components\Payments;
+
+use WirecardShopwareElasticEngine\Components\Payments\Payment;
+
+class AbstractPaymentTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Payment */
+    protected $stub;
+
+    public function setUp()
+    {
+        $this->stub = $this->getMockForAbstractClass(Payment::class, [], 'FooPayment');
+    }
+
+    public function testInstanceOfPaymentInterface()
+    {
+        $this->assertInstanceOf('WirecardShopwareElasticEngine\Components\Payments\PaymentInterface', $this->stub);
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals('wirecard_ee_foo', $this->stub->getName());
+    }
+
+    public function testGetLabel()
+    {
+        $this->assertEquals('Wirecard EE Foo', $this->stub->getLabel());
+    }
+
+    public function testGetPaymentOptions()
+    {
+        $this->assertTrue(is_array($this->stub->getPaymentOptions()));
+        $this->assertTrue(array_key_exists('name', $this->stub->getPaymentOptions()));
+        $this->assertTrue(array_key_exists('description', $this->stub->getPaymentOptions()));
+        $this->assertTrue(array_key_exists('action', $this->stub->getPaymentOptions()));
+        $this->assertTrue(array_key_exists('active', $this->stub->getPaymentOptions()));
+        $this->assertTrue(array_key_exists('position', $this->stub->getPaymentOptions()));
+        $this->assertTrue(array_key_exists('additionalDescription', $this->stub->getPaymentOptions()));
+    }
+}
