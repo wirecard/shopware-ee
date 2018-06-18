03af5e2a51755b7104f4f7790809087d7d1b6e3c
nehalist <github@nehalist.io>
2018-06-18 15:04:59 +0200

    Fixed namespaces

--- Tests/Functional/ExampleTest.php
+++ Tests/Functional/ExampleTest.php
@@ -29,7 +29,7 @@
  * Please do not use the plugin if you do not agree to these terms of use!
  */
 
-namespace WirecardElasticEngine\Tests\Functional;
+namespace WirecardShopwareElasticEngine\Tests\Functional;
 
 use Shopware\Components\Test\Plugin\TestCase;
 use Wirecard\PaymentSdk\Config\Config;
--- Tests/Unit/DefaultTest.php
+++ Tests/Unit/DefaultTest.php
@@ -29,7 +29,7 @@
  * Please do not use the plugin if you do not agree to these terms of use!
  */
 
-namespace WirecardElasticEngine\Tests\Unit;
+namespace WirecardShopwareElasticEngine\Tests\Unit;
 
 class DefaultTest extends \PHPUnit_Framework_TestCase
 {
