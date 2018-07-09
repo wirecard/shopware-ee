b09facf2821fc21586ba764db95244ab70069877
Daniel Leeb <dl@loremipsum.at>
2018-07-09 18:38:02 +0200

    Fix subscriber tests namespace

--- Tests/Unit/Subscriber/FrontendSubscriberTest.php
+++ Tests/Unit/Subscriber/FrontendSubscriberTest.php
@@ -29,7 +29,7 @@
  * Please do not use the plugin if you do not agree to these terms of use!
  */
 
-namespace WirecardShopwareElasticEngine\Tests\Unit\Components\Data;
+namespace WirecardShopwareElasticEngine\Tests\Unit\Subscriber;
 
 use PHPUnit\Framework\TestCase;
 use WirecardShopwareElasticEngine\Subscriber\FrontendSubscriber;
