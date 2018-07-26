b5ed74b1a86013f90f5f91f843a0211892919cce
nehalist <github@nehalist.io>
2018-07-26 13:46:03 +0200

    Added payment contracts

---
+++ Components/Payments/Contracts/ProcessPaymentInterface.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Shop System Plugins - Terms of Use
+ *
+ * The plugins offered are provided free of charge by Wirecard AG and are explicitly not part
+ * of the Wirecard AG range of products and services.
+ *
+ * They have been tested and approved for full functionality in the standard configuration
+ * (status on delivery) of the corresponding shop system. They are under General Public
+ * License version 3 (GPLv3) and can be used, developed and passed on to third parties under
+ * the same terms.
+ *
+ * However, Wirecard AG does not provide any guarantee or accept any liability for any errors
+ * occurring when used in an enhanced, customized shop system configuration.
+ *
+ * Operation in an enhanced, customized configuration is at your own risk and requires a
+ * comprehensive test phase by the user of the plugin.
+ *
+ * Customers use the plugins at their own risk. Wirecard AG does not guarantee their full
+ * functionality neither does Wirecard AG assume liability for any disadvantages related to
+ * the use of the plugins. Additionally, Wirecard AG does not guarantee the full functionality
+ * for customized shop systems or installed plugins of other vendors of plugins within the same
+ * shop system.
+ *
+ * Customers are responsible for testing the plugin's functionality before starting productive
+ * operation.
+ *
+ * By installing the plugin into the shop system the customer agrees to these terms of use.
+ * Please do not use the plugin if you do not agree to these terms of use!
+ */
+
+namespace WirecardShopwareElasticEngine\Components\Payments\Contracts;
+
+use Shopware\Models\Shop\Shop;
+use Wirecard\PaymentSdk\Entity\Redirect;
+use Wirecard\PaymentSdk\TransactionService;
+use WirecardShopwareElasticEngine\Components\Actions\Action;
+use WirecardShopwareElasticEngine\Components\Data\OrderSummary;
+use WirecardShopwareElasticEngine\Components\Services\PaymentHandler;
+
+/**
+ * @package WirecardShopwareElasticEngine\Components\Payments\Interfaces
+ *
+ * @since   1.0.0
+ */
+interface ProcessPaymentInterface
+{
+    /**
+     * Payment specific processing. This method either returns an `Action` (which is directly returned to the Handler)
+     * or `null`. Returning `null` leads to the handler executing the transaction via the `TransactionService`. In case
+     * of returning an `Action` execution of the transaction (via the `TransactionService`) probably needs to get
+     * called manually within this method.
+     *
+     * @see   PaymentHandler
+     *
+     * @param OrderSummary                        $orderSummary
+     * @param TransactionService                  $transactionService
+     * @param Shop                                $shop
+     * @param Redirect                            $redirect
+     * @param \Enlight_Controller_Request_Request $request
+     * @param \sOrder                             $shopwareOrder
+     *
+     * @return Action|null
+     *
+     * @since 1.0.0
+     */
+    public function processPayment(
+        OrderSummary $orderSummary,
+        TransactionService $transactionService,
+        Shop $shop,
+        Redirect $redirect,
+        \Enlight_Controller_Request_Request $request,
+        \sOrder $shopwareOrder
+    );
+}
