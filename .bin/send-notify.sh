1c7975753d64654c9f73ac3835cb2f7aec48b533
adis.brkic <adis.brkic@wirecard.com>
2019-03-27 15:48:30 +0100

    Add after failure and delete empty space

--- .bin/send-notify.sh
+++ .bin/send-notify.sh
@@ -7,9 +7,3 @@ curl -X POST -H 'Content-type: application/json' \
     --data "{'text': 'Build Failed. Build URL : ${TRAVIS_JOB_WEB_URL}\n
     Build Number: ${TRAVIS_BUILD_NUMBER}\n
     Branch: ${TRAVIS_BRANCH}', 'channel': '${CHANNEL}'}" ${SLACK_ROOMS}
-
-
-
-
-
-
