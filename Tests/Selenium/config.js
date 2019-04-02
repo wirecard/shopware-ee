978ab345b857ea42033e9a01e6f18cab59226841
adis.brkic <adis.brkic@wirecard.com>
2019-04-02 07:25:51 +0200

    Uncomment all browsers and remove shell

--- Tests/Selenium/config.js
+++ Tests/Selenium/config.js
@@ -182,118 +182,118 @@ exports.browsers = [
         os: WINDOWS.name,
         os_version: WINDOWS.versions.win10,
         resolution: DEFAULT_RESOLUTION
+    },
+    {
+        browserName: FIREFOX.name,
+        browser_version: FIREFOX.currentVersion,
+        os: WINDOWS.name,
+        os_version: WINDOWS.versions.win8,
+        resolution: DEFAULT_RESOLUTION
+    },
+    {
+        browserName: OPERA.name,
+        browser_version: OPERA.currentVersion,
+        os: WINDOWS.name,
+        os_version: WINDOWS.versions.win8,
+        resolution: DEFAULT_RESOLUTION
+    },
+    {
+        browserName: IE.name,
+        browser_version: IE.versions.ie8,
+        os: WINDOWS.name,
+        os_version: WINDOWS.versions.win7,
+        resolution: DEFAULT_RESOLUTION
+    },
+    {
+        browserName: IE.name,
+        browser_version: IE.versions.ie9,
+        os: WINDOWS.name,
+        os_version: WINDOWS.versions.win7,
+        resolution: DEFAULT_RESOLUTION
+    },
+    {
+        browserName: IE.name,
+        browser_version: IE.versions.ie10,
+        os: WINDOWS.name,
+        os_version: WINDOWS.versions.win7,
+        resolution: DEFAULT_RESOLUTION
+    },
+    {
+        browserName: IE.name,
+        browser_version: IE.versions.ie11,
+        os: WINDOWS.name,
+        os_version: WINDOWS.versions.win7,
+        resolution: DEFAULT_RESOLUTION
+    },
+    // APPLE
+    {
+        browserName: CHROME.name,
+        browser_version: CHROME.currentVersion,
+        os: OSX.name,
+        os_version: OSX.versions.highSierra,
+        resolution: DEFAULT_RESOLUTION
+    },
+    {
+        browserName: CHROME.name,
+        browser_version: CHROME.currentVersion,
+        os: OSX.name,
+        os_version: OSX.versions.sierra,
+        resolution: DEFAULT_RESOLUTION
+    },
+    {
+        browserName: SAFARI.name,
+        browser_version: SAFARI.versions.v11_1,
+        os: OSX.name,
+        os_version: OSX.versions.highSierra,
+        resolution: DEFAULT_RESOLUTION
+    },
+    {
+        browserName: SAFARI.name,
+        browser_version: SAFARI.versions.v10_1,
+        os: OSX.name,
+        os_version: OSX.versions.sierra,
+        resolution: DEFAULT_RESOLUTION
+    },
+    // MOBILE: ANDROID
+    {
+        browserName: CHROME.name,
+        os: ANDROID_7_DEVICE.name,
+        os_version: ANDROID_7_DEVICE.version,
+        real_mobile: 'true'
+    },
+    {
+        browserName: CHROME.name,
+        os: ANDROID_8_DEVICE.name,
+        os_version: ANDROID_8_DEVICE.version,
+        real_mobile: 'true'
+    },
+    {
+        browserName: CHROME.name,
+        device: ANDROID_8_DEVICE.name,
+        os_version: ANDROID_8_DEVICE.version,
+        real_mobile: 'true',
+        deviceOrientation: 'landscape'
+    },
+    // MOBILE: iOS
+    {
+        browserName: SAFARI.name,
+        os: IOS_10_DEVICE.name,
+        os_version: IOS_10_DEVICE.version,
+        real_mobile: 'true'
+    },
+    {
+        browserName: SAFARI.name,
+        os: IOS_11_DEVICE.name,
+        os_version: IOS_11_DEVICE.version,
+        real_mobile: 'true'
+    },
+    {
+        browserName: SAFARI.name,
+        os: IOS_11_DEVICE.name,
+        os_version: IOS_11_DEVICE.version,
+        real_mobile: 'true',
+        deviceOrientation: 'landscape'
     }
-    // {
-    //     browserName: FIREFOX.name,
-    //     browser_version: FIREFOX.currentVersion,
-    //     os: WINDOWS.name,
-    //     os_version: WINDOWS.versions.win8,
-    //     resolution: DEFAULT_RESOLUTION
-    // },
-    // {
-    //     browserName: OPERA.name,
-    //     browser_version: OPERA.currentVersion,
-    //     os: WINDOWS.name,
-    //     os_version: WINDOWS.versions.win8,
-    //     resolution: DEFAULT_RESOLUTION
-    // },
-    // {
-    //     browserName: IE.name,
-    //     browser_version: IE.versions.ie8,
-    //     os: WINDOWS.name,
-    //     os_version: WINDOWS.versions.win7,
-    //     resolution: DEFAULT_RESOLUTION
-    // },
-    // {
-    //     browserName: IE.name,
-    //     browser_version: IE.versions.ie9,
-    //     os: WINDOWS.name,
-    //     os_version: WINDOWS.versions.win7,
-    //     resolution: DEFAULT_RESOLUTION
-    // },
-    // {
-    //     browserName: IE.name,
-    //     browser_version: IE.versions.ie10,
-    //     os: WINDOWS.name,
-    //     os_version: WINDOWS.versions.win7,
-    //     resolution: DEFAULT_RESOLUTION
-    // },
-    // {
-    //     browserName: IE.name,
-    //     browser_version: IE.versions.ie11,
-    //     os: WINDOWS.name,
-    //     os_version: WINDOWS.versions.win7,
-    //     resolution: DEFAULT_RESOLUTION
-    // },
-    // // APPLE
-    // {
-    //     browserName: CHROME.name,
-    //     browser_version: CHROME.currentVersion,
-    //     os: OSX.name,
-    //     os_version: OSX.versions.highSierra,
-    //     resolution: DEFAULT_RESOLUTION
-    // },
-    // {
-    //     browserName: CHROME.name,
-    //     browser_version: CHROME.currentVersion,
-    //     os: OSX.name,
-    //     os_version: OSX.versions.sierra,
-    //     resolution: DEFAULT_RESOLUTION
-    // },
-    // {
-    //     browserName: SAFARI.name,
-    //     browser_version: SAFARI.versions.v11_1,
-    //     os: OSX.name,
-    //     os_version: OSX.versions.highSierra,
-    //     resolution: DEFAULT_RESOLUTION
-    // },
-    // {
-    //     browserName: SAFARI.name,
-    //     browser_version: SAFARI.versions.v10_1,
-    //     os: OSX.name,
-    //     os_version: OSX.versions.sierra,
-    //     resolution: DEFAULT_RESOLUTION
-    // },
-    // // MOBILE: ANDROID
-    // {
-    //     browserName: CHROME.name,
-    //     os: ANDROID_7_DEVICE.name,
-    //     os_version: ANDROID_7_DEVICE.version,
-    //     real_mobile: 'true'
-    // },
-    // {
-    //     browserName: CHROME.name,
-    //     os: ANDROID_8_DEVICE.name,
-    //     os_version: ANDROID_8_DEVICE.version,
-    //     real_mobile: 'true'
-    // },
-    // {
-    //     browserName: CHROME.name,
-    //     device: ANDROID_8_DEVICE.name,
-    //     os_version: ANDROID_8_DEVICE.version,
-    //     real_mobile: 'true',
-    //     deviceOrientation: 'landscape'
-    // },
-    // // MOBILE: iOS
-    // {
-    //     browserName: SAFARI.name,
-    //     os: IOS_10_DEVICE.name,
-    //     os_version: IOS_10_DEVICE.version,
-    //     real_mobile: 'true'
-    // },
-    // {
-    //     browserName: SAFARI.name,
-    //     os: IOS_11_DEVICE.name,
-    //     os_version: IOS_11_DEVICE.version,
-    //     real_mobile: 'true'
-    // },
-    // {
-    //     browserName: SAFARI.name,
-    //     os: IOS_11_DEVICE.name,
-    //     os_version: IOS_11_DEVICE.version,
-    //     real_mobile: 'true',
-    //     deviceOrientation: 'landscape'
-    // }
 ];
 
 /**
@@ -302,53 +302,43 @@ exports.browsers = [
 exports.tests = [
     {
         file: 'Payments/DefaultTest',
-        timeout: 30000
-        // timeout: 120000
+        timeout: 120000
     },
     {
         file: 'Payments/CreditCardTest',
-        timeout: 30000
-        // timeout: 120000
+        timeout: 120000
     },
     {
         file: 'Payments/CreditCardThreeDTest',
-        timeout: 30000
-        // timeout: 120000
+        timeout: 120000
     },
     {
         file: 'Payments/AlipayTest',
-        timeout: 30000
-        // timeout: 120000
+        timeout: 120000
     },
     {
         file: 'Payments/RatepayTest',
-        timeout: 30000
-        // timeout: 120000
+        timeout: 120000
     },
     {
         file: 'Payments/IdealTest',
-        timeout: 30000
-        // timeout: 120000
+        timeout: 120000
     },
     {
         file: 'Payments/MasterpassTest',
-        timeout: 30000
-        // timeout: 120000
+        timeout: 120000
     },
     {
         file: 'Payments/PaypalTest',
-        timeout: 30000
-        // timeout: 180000
+        timeout: 180000
     },
     {
         file: 'Payments/PiaTest',
-        timeout: 30000
-        // timeout: 90000
+        timeout: 90000
     },
     {
         file: 'Payments/PoiTest',
-        timeout: 30000
-        // timeout: 90000
+        timeout: 90000
     },
     {
         file: 'Payments/SepaTest',
@@ -356,12 +346,10 @@ exports.tests = [
     },
     {
         file: 'Payments/SofortTest',
-        timeout: 30000
-        // timeout: 120000
+        timeout: 120000
     },
     {
         file: 'Payments/UpiTest',
-        timeout: 30000
-        // timeout: 120000
+        timeout: 120000
     }
 ];
