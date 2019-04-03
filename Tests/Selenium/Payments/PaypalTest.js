c4c82037046f3afe1842a9d439f1c84de19826f2
adis.brkic <adis.brkic@wirecard.com>
2019-04-03 08:00:23 +0200

    Add wait before element

--- Tests/Selenium/Payments/PaypalTest.js
+++ Tests/Selenium/Payments/PaypalTest.js
@@ -38,9 +38,10 @@ describe('PayPal test', () => {
         try {
             // Log in to PayPal
             // console.log('wait for .maskable');
-            // console.log('wait for #content');
+            console.log('wait for #email');
             await driver.wait(until.elementLocated(By.id('email')), 10000);
             await driver.findElement(By.id('email')).sendKeys(formFields.email, Key.ENTER);
+            console.log('wait for #password');
             await driver.wait(until.elementLocated(By.id('password')), 10000);
             await driver.findElement(By.id('password')).sendKeys(formFields.password, Key.ENTER);
             await driver.findElement(By.css('btnLogin')).click();
