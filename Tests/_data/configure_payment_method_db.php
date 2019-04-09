20a4ae6b89a40b4af52d3be7b654da13531660d0
adis.brkic@wirecard.com <Moja.bosna10>
2019-04-09 08:07:20 +0200

    Comment select from query

--- Tests/_data/configure_payment_method_db.php
+++ Tests/_data/configure_payment_method_db.php
@@ -174,13 +174,13 @@ function updateShopwareEeDbConfig($db_config, $payment_method)
         }
     }
 
-    echo "New Database rows!\n";
-    $stmtInsert = "SELECT * FROM $tableName";
-    $result = $mysqli->query($stmtInsert);
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            echo $row['name'] . " " . $row['value'] . "\n";
-        }
-    }
+//    echo "New Database rows!\n";
+//    $stmtInsert = "SELECT * FROM $tableName";
+//    $result = $mysqli->query($stmtInsert);
+//    if ($result->num_rows > 0) {
+//        while($row = $result->fetch_assoc()) {
+//            echo $row['name'] . " " . $row['value'] . "\n";
+//        }
+//    }
     return true;
 }
