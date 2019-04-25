a813f7d83b5cabab98837f93666fb2906fce6434
Tatjana <tatjana.starcenko@wirecard.com>
2019-04-25 15:40:39 +0200

    Set env variable from file in travis

--- .bin/get-shopware-versions.sh
+++ .bin/get-shopware-versions.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 #get last 3  releases
-curl --silent "https://api.github.com/repos/shopware/shopware/releases" | jq -r '.[] | .tag_name' | head -3 > .bin/shop-releases.txt
+curl --silent "https://api.github.com/repos/shopware/shopware/releases" | jq -r '.[] | .tag_name' | head -3 > ${SHOPWARE_RELEASES_FILE}
 git config --global user.name "Travis CI"
 git config --global user.email "wirecard@travis-ci.org"
 
 git add .bin/shop-releases.txt
 git commit -m "[skip ci] Update latest shop releases"
-git push --quiet https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG HEAD:TPWDCEE-3662
-#awk 'NR==1 {print; exit}' releases.txt
+git push --quiet https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} HEAD:TPWDCEE-3662
