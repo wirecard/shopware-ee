2ac75dc9b9d44f8df75cf226bed24614df4db779
Tatjana <tatjana.starcenko@wirecard.com>
2019-05-13 15:48:29 +0200

    Add Shopware version to notifications and uncomment sending notifications

--- .bin/send-notify.sh
+++ .bin/send-notify.sh
@@ -8,6 +8,7 @@ fi
 
 #send information about the build
 curl -X POST -H 'Content-type: application/json' \
-    --data "{'text': 'Build Failed. Build URL : ${TRAVIS_JOB_WEB_URL}\n
+    --data "{'text': 'Build Failed. Shopware version: ${SHOP_RELEASE}\n
+    Build URL : ${TRAVIS_JOB_WEB_URL}\n
     Build Number: ${TRAVIS_BUILD_NUMBER}\n
     Branch: ${TRAVIS_BRANCH}', 'channel': '${CHANNEL}'}" ${SLACK_ROOMS}
