fd4d76d9cc1a281f65d23e607528701f9ac585b3
Adis Brkic <35455551+brkicadis@users.noreply.github.com>
2019-08-07 13:04:43 +0200

    Enable One-Click Checkout and disable nightly running (#113)
    * Enable One-Click Checkout and disable nightly running
    
    * Improve Credit Card Test
    
    * Remove vault test
    
    * Add try catch to credit card three ds
    
    * Enable nightly running and push versions to master branch

--- Tests/Selenium/Payments/CreditCardThreeDTest.js
+++ Tests/Selenium/Payments/CreditCardThreeDTest.js
@@ -35,38 +35,77 @@ describe('Credit Card 3-D Secure test', () => {
         await addProductToCartAndGotoCheckout(driver, '/wohnwelten/moebel/68/kommode-shabby-chic');
         await selectPaymentMethod(driver, paymentLabel);
 
-        // Confirm order
-        console.log('click button confirm--form');
-        await driver.findElement(By.xpath('//button[@form="confirm--form"]')).click();
+        try {
+            // Do not use saved card
+            console.log('click #wirecardee--token-no-card');
+            await driver.findElement(By.id('wirecardee--token-no-card')).click();
 
-        // Fill out credit card iframe
-        console.log('wait for .wirecard-seamless-frame');
-        await driver.wait(until.elementLocated(By.className('wirecard-seamless-frame')), 20000);
-        console.log('wait for switch to iframe .wirecard-seamless-frame');
-        await driver.wait(until.ableToSwitchToFrame(By.className('wirecard-seamless-frame')));
-        console.log('wait for #account_number');
-        await driver.wait(until.elementLocated(By.id('account_number')), 20000);
-        await asyncForEach(Object.keys(formFields), async field => {
-            await driver.findElement(By.id(field)).sendKeys(formFields[field]);
-        });
-        await driver.findElement(By.css('#expiration_month_list > option[value=\'01\']')).click();
-        await driver.findElement(By.css('#expiration_year_list > option[value=\'2023\']')).click();
+            // Confirm order
+            console.log('click button confirm--form');
+            await driver.findElement(By.xpath('//button[@form="confirm--form"]')).click();
 
-        // Switch back from iframe and click Send button
-        console.log('switch back from iframe to default content');
-        await driver.switchTo().defaultContent();
-        console.log('wait for #wirecardee-credit-card--form-submit');
-        await driver.wait(until.elementLocated(By.id('wirecardee-credit-card--form-submit')), 10000);
-        console.log('click #wirecardee-credit-card--form-submit');
-        await driver.findElement(By.id('wirecardee-credit-card--form-submit')).click();
+            // Fill out credit card iframe
+            console.log('wait for .wirecard-seamless-frame');
+            await driver.wait(until.elementLocated(By.className('wirecard-seamless-frame')), 20000);
+            console.log('wait for switch to iframe .wirecard-seamless-frame');
+            await driver.wait(until.ableToSwitchToFrame(By.className('wirecard-seamless-frame')));
+            console.log('wait for #account_number');
+            await driver.wait(until.elementLocated(By.id('account_number')), 20000);
+            await asyncForEach(Object.keys(formFields), async field => {
+                await driver.findElement(By.id(field)).sendKeys(formFields[field]);
+            });
+            await driver.findElement(By.css('#expiration_month_list > option[value=\'01\']')).click();
+            await driver.findElement(By.css('#expiration_year_list > option[value=\'2023\']')).click();
 
-        console.log('wait for #password');
-        await driver.wait(until.elementLocated(By.id('password')), 20000);
-        await driver.findElement(By.id('password')).sendKeys(config.payments.creditCardThreeD.password, Key.ENTER);
+            // Switch back from iframe and click Send button
+            console.log('switch back from iframe to default content');
+            await driver.switchTo().defaultContent();
+            console.log('wait for #wirecardee-credit-card--form-submit');
+            await driver.wait(until.elementLocated(By.id('wirecardee-credit-card--form-submit')), 10000);
+            console.log('click #wirecardee-credit-card--form-submit');
+            await driver.findElement(By.id('wirecardee-credit-card--form-submit')).click();
 
-        await waitForAlert(driver, 20000);
+            console.log('wait for #password');
+            await driver.wait(until.elementLocated(By.id('password')), 20000);
+            await driver.findElement(By.id('password')).sendKeys(config.payments.creditCardThreeD.password, Key.ENTER);
 
-        await checkConfirmationPage(driver, paymentLabel);
+            await waitForAlert(driver, 20000);
+
+            await checkConfirmationPage(driver, paymentLabel);
+        } catch (e) {
+            // Confirm order
+            console.log('click button confirm--form');
+            await driver.findElement(By.xpath('//button[@form="confirm--form"]')).click();
+
+            // Fill out credit card iframe
+            console.log('wait for .wirecard-seamless-frame');
+            await driver.wait(until.elementLocated(By.className('wirecard-seamless-frame')), 20000);
+            console.log('wait for switch to iframe .wirecard-seamless-frame');
+            await driver.wait(until.ableToSwitchToFrame(By.className('wirecard-seamless-frame')));
+            console.log('wait for #account_number');
+            await driver.wait(until.elementLocated(By.id('account_number')), 20000);
+            await asyncForEach(Object.keys(formFields), async field => {
+                await driver.findElement(By.id(field)).sendKeys(formFields[field]);
+            });
+            await driver.findElement(By.css('#expiration_month_list > option[value=\'01\']')).click();
+            await driver.findElement(By.css('#expiration_year_list > option[value=\'2023\']')).click();
+
+            // Switch back from iframe and click Send button
+            console.log('switch back from iframe to default content');
+            await driver.switchTo().defaultContent();
+            console.log('wait for #wirecardee-credit-card--form-submit');
+            await driver.wait(until.elementLocated(By.id('wirecardee-credit-card--form-submit')), 10000);
+            console.log('click #wirecardee-credit-card--form-submit');
+            await driver.findElement(By.id('wirecardee-credit-card--form-submit')).click();
+
+            console.log('wait for #password');
+            await driver.wait(until.elementLocated(By.id('password')), 20000);
+            await driver.findElement(By.id('password')).sendKeys(config.payments.creditCardThreeD.password, Key.ENTER);
+
+            await waitForAlert(driver, 20000);
+
+            await checkConfirmationPage(driver, paymentLabel);
+        }
     });
 
     after(async () => driver.quit());
