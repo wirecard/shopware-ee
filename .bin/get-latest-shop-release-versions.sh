491638279b1e020f78167d36e4f539d4149ceb5e
Adis Brkic <35455551+brkicadis@users.noreply.github.com>
2019-10-31 11:41:00 +0100

    Check compatibility and send notification in order to appropriate behaviour (#131)

--- .bin/get-latest-shop-release-versions.sh
+++ .bin/get-latest-shop-release-versions.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-#get last 3  releases
+set -e
+set -x
+
+# get last 3  releases
 curl -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/shopware/shopware/releases | jq -r '.[] | .tag_name' | egrep -v [A-Z] | head -3 > tmp.txt
 git config --global user.name "Travis CI"
 git config --global user.email "wirecard@travis-ci.org"
 
-sort -nr tmp.txt > ${SHOPWARE_RELEASES_FILE}
+sort -nr tmp.txt > ${SHOPWARE_COMPATIBILITY_FILE}
 
-if [[ $(git diff HEAD ${SHOPWARE_RELEASES_FILE}) != '' ]]; then
-    git add  ${SHOPWARE_RELEASES_FILE}
+if [[ $(git diff HEAD ${SHOPWARE_COMPATIBILITY_FILE}) != '' ]]; then
+    git add  ${SHOPWARE_COMPATIBILITY_FILE}
     git commit -m "${SHOP_SYSTEM_UPDATE_COMMIT}"
     git push --quiet https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG} HEAD:master
 fi
