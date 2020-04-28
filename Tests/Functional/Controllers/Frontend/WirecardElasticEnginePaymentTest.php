839a740c35bf3dc44d2dc98cb5a553ffbb2616a9
Flavius Aspra <flavius.aspra@wirecard.com>
2020-04-28 15:21:39 +0200

    Disable test

--- Tests/Functional/Controllers/Frontend/WirecardElasticEnginePaymentTest.php
+++ Tests/Functional/Controllers/Frontend/WirecardElasticEnginePaymentTest.php
@@ -27,7 +27,7 @@ class WirecardElasticEnginePaymentTest extends \Enlight_Components_Test_Plugin_T
         $this->reset();
     }
 
-    public function _testIndexAction()
+    public function disabled_testIndexAction()
     {
         $basketData = require __DIR__ . '/testdata/index-basket.php';
 
